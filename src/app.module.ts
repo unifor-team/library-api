688cc0afb7fe9b0338a7aada3cb65697d6debea3
Manelitu <emanuelwork1@gmail.com>
2024-03-10 12:50:55 -0300

    feat: insert a config module when i use .env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BookModule, EditionModule, PostModule, UserModule } from './modules';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, BookModule, EditionModule, PostModule],
+  imports: [UserModule, BookModule, EditionModule, PostModule, ConfigModule.forRoot({
+    envFilePath: '.env',
+  })],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.DEFAULT_PORT;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
+
 bootstrap();
