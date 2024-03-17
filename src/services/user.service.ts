70dca732b182ee85f77f9b78a7de268e7e8c7d17
Manelitu <emanuelwork1@gmail.com>
2024-03-17 17:37:05 -0300

    fix: user already exist

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -14,54 +14,74 @@ export class UserService {
     try {
       const { email, password, name } = user;
       const newUser = await User.build(email, password, name);
-      const emailExist = this.userRepository.findByEmail(email);
+      const emailExist = await this.userRepository.findByEmail(email);
       if (emailExist) throw new HttpException('Email already exists', HttpStatus.CONFLICT);
       return this.userRepository.create(newUser);
     } catch (err) {
-      console.log(err)
+      return err;
     }
   }
 
   async update(id: string, user: EditUserDTO) {
-    if (!id) throw new HttpException('Bad request', HttpStatus.BAD_REQUEST);
+    try {
+      if (!id) throw new HttpException('Bad request', HttpStatus.BAD_REQUEST);
 
-    const existedUser = await this.userRepository.listById(id);
+      const existedUser = await this.userRepository.listById(id);
 
-    if (!existedUser) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      if (!existedUser) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
 
-    const { email, password, name } = user;
-    const updatedUser = await User.build(email, password, name);
+      const { email, password, name } = user;
+      const updatedUser = await User.build(email, password, name);
 
-    updatedUser.identifier = existedUser.id;
-    updatedUser.created_at = existedUser.created_at;
-    updatedUser.updatedAt = new Date();
-    return this.userRepository.update(id, updatedUser);
+      updatedUser.identifier = existedUser.id;
+      updatedUser.created_at = existedUser.created_at;
+      updatedUser.updatedAt = new Date();
+      return this.userRepository.update(id, updatedUser);
+    } catch (err) {
+      return err;
+    }
   }
 
   async listAll() {
-    return this.userRepository.list();
+    try {
+      return this.userRepository.list();
+    } catch (err) {
+      return err;
+    }
   }
 
   async listById(id: string) {
-    if (!id) throw new HttpException('Bad request', HttpStatus.BAD_REQUEST);
-    const existedUser = await this.userRepository.listById(id);
-    if (!existedUser) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-    return existedUser;
+    try {
+      if (!id) throw new HttpException('Bad request', HttpStatus.BAD_REQUEST);
+      const existedUser = await this.userRepository.listById(id);
+      if (!existedUser) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      return existedUser;
+    } catch (err) {
+      return err
+    }
   }
 
   async findUserByEmail(email: string) {
-    if (!email) throw new HttpException('Wrong data', HttpStatus.BAD_REQUEST);
-    const userExist = await this.userRepository.findByEmail(email);
-    if (!userExist) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-    return userExist;
+    try {
+      if (!email) throw new HttpException('Wrong data', HttpStatus.BAD_REQUEST);
+      const userExist = await this.userRepository.findByEmail(email);
+      if (!userExist) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      return userExist;
+    } catch (err) {
+      return err;
+    }
   }
 
   async delete(id: string) {
-    if (!id) throw new HttpException('Bad request', HttpStatus.BAD_REQUEST);
-    const existedUser = await this.userRepository.listById(id);
-    if (!existedUser) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-    const user = { ...existedUser, account_status: AccountStatus.DISABLED }
-    user.deleted_at = new Date();
-    return this.userRepository.delete(id, user);
+    try {
+      if (!id) throw new HttpException('Bad request', HttpStatus.BAD_REQUEST);
+      const existedUser = await this.userRepository.listById(id);
+      if (!existedUser) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      const user = { ...existedUser, account_status: AccountStatus.DISABLED }
+      user.deleted_at = new Date();
+      return this.userRepository.delete(id, user);
+    } catch (err) {
+      return err
+    }
   }
 }
